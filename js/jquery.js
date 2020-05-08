d5eab66299d3111350e0a642eac0e271833c9f0a
Osman Demirci <osmanndmrc@gmail.com>
2020-05-09 00:49:44 +0300

    Update jquery.js

--- js/jquery.js
+++ js/jquery.js
@@ -1,5 +1,5 @@
 /*! jQuery v1.9.1 | (c) 2005, 2012 jQuery Foundation, Inc. | jquery.org/license
- //@ sourceMappingURL=jquery.min.map
+ //# sourceMappingURL=jquery.min.map
  */
 (function(e, t) {
     var n, r, i = typeof t,
